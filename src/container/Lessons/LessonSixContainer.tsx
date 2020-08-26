4e11f7098ace5d2e02aeb611cb1f4641de243a04
james.olmedo <james.olmedo@infivex.com>
2020-08-27 00:46:53 +0800

    lesson 6

--- src/container/Lessons/LessonSixContainer.tsx
+++ src/container/Lessons/LessonSixContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 
@@ -12,11 +12,13 @@ export default () => {
   const hideTabBar = () => dispatch(customNavigation.onHideTab());
   const showTabBar = () => dispatch(customNavigation.onShowTab());
 
-  return (
-    <LessonSix
-      hideTabBar={hideTabBar}
-      showTabBar={showTabBar}
-      navigation={navigation}
-    />
-  );
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('beforeRemove', () => {
+      showTabBar();
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  return <LessonSix hideTabBar={hideTabBar} showTabBar={showTabBar} />;
 };
